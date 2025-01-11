8ef605c681d762ea50c5c02717d16e0713972b0d
Emberizaf <1522236760@qq.com>
2025-01-11 20:41:07 +0800

    RedisUtils工具类

--- music-server/src/test/java/com/music/common/common/test.java
+++ music-server/src/test/java/com/music/common/common/test.java
@@ -2,10 +2,12 @@ package com.music.common.common;
 
 import com.music.common.MusicommunityApplication;
 import com.music.common.common.utils.JwtUtils;
+import com.music.common.common.utils.RedisUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = MusicommunityApplication.class)
@@ -20,5 +22,15 @@ public class test {
         System.out.println(jwtUtils.createToken(1L));
         System.out.println(jwtUtils.createToken(2L));
         jwtUtils.createToken(2L);
+    }
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void redis() {
+        RedisUtils.set("name","卷心菜");
+        String name = RedisUtils.get("name");
+        System.out.println(name); //卷心菜
     }
 }
