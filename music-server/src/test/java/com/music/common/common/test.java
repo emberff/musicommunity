5acf918c5150433928a4077920bdb42beda14987
Emberizaf <1522236760@qq.com>
2025-01-11 20:47:05 +0800

    Redisson分布式锁

--- music-server/src/test/java/com/music/common/common/test.java
+++ music-server/src/test/java/com/music/common/common/test.java
@@ -5,6 +5,8 @@ import com.music.common.common.utils.JwtUtils;
 import com.music.common.common.utils.RedisUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -32,5 +34,16 @@ public class test {
         RedisUtils.set("name","卷心菜");
         String name = RedisUtils.get("name");
         System.out.println(name); //卷心菜
+    }
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Test
+    public void redisson() {
+        RLock lock = redissonClient.getLock("123");
+        lock.lock();
+        System.out.println();
+        lock.unlock();
     }
 }
