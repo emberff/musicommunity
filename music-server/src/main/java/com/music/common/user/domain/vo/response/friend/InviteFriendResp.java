21ce1b877fe8af57cb09fa0e9f8527edd02124f2
Emberizaf <1522236760@qq.com>
2025-05-30 11:00:26 +0800

    修改 todo: √1.歌手详情页(按专辑查询分页) 2.上传作品页: 两种类型, 上传歌曲至曲库 发帖子 3.歌手发布作品ws接入 4.帖子页详情查看, 评论 √5.歌单管理与群聊

---
+++ music-server/src/main/java/com/music/common/user/domain/vo/response/friend/InviteFriendResp.java
@@ -0,0 +1,24 @@
+package com.music.common.user.domain.vo.response.friend;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * Description: 好友校验
+ * Author: <a href="https://github.com/emberff">pf</a>
+ * Date: 2023-03-23
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InviteFriendResp extends FriendResp{
+
+    @ApiModelProperty("是否已被邀请")
+    private Integer isInvited;
+    
+}
